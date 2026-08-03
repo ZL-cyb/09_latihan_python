98d86e4e4c09e0704760028ba2850c9c24f5badd
ZL-cyb <fahim.tkjsmeksa@gmail.com>
2026-08-03 14:51:50 +0700

    Tugas Tugas

---
+++ cek_ganjil_genap.py
@@ -0,0 +1,13 @@
+# OPERATOR GANJIL GENAP
+print("=" * 30)
+print("PROGRAM CEK GANJIL/GENAP")
+
+angka = int(input("Masukan angka :"))
+
+if angka % 2 == 0:
+    print(f"{angka} adalah bilangan GENAP")
+else:
+    print(f"{angka} adalah bilangan GANJIL")
+    
+print("=" * 30 )
+print("SELESAI")
---
+++ input_rumus_persegi.py
@@ -0,0 +1,9 @@
+# RUMUS LUAS PERSEGI PANJANG
+
+P =  int(input("masukan panjang pesegi: "))
+L = int(input("masukan lebar persegi:"))
+
+a = P*L
+
+print(a)
+         
