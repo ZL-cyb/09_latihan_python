9eae1f4f94728ffbb6881776a6716c616f7c0a4e
ZL-cyb <fahim.tkjsmeksa@gmail.com>
2026-08-10 13:53:29 +0700

    penambahan looping

--- cek_ganjil_genap.py
+++ cek_ganjil_genap.py
@@ -1,13 +1,24 @@
 # OPERATOR GANJIL GENAP
 print("=" * 30)
 print("PROGRAM CEK GANJIL/GENAP")
+print("=" * 30)
 
-angka = int(input("Masukan angka :"))
-
-if angka % 2 == 0:
-    print(f"{angka} adalah bilangan GENAP")
-else:
-    print(f"{angka} adalah bilangan GANJIL")
+while True:
+    #meminta input angka dari user
+    angka= int (input("\nMasukan angka :"))
+    
+    #mengecek apakah genap atau ganjil
+    if angka % 2 == 0:
+        print(f"{angka} adalah bilangan GENAP")
+    else:
+        print(f"{angka} adalah bilangan GANJIL")
+        
+    #tanya user ingin mengulang?
+    ulang= input("Apakah mau cek angka lagi? (y/n): ")
+    
+    #jika user mengetik selain 'y' loop akan berhenti
+    if ulang.lower() != 'y':
+        break
     
 print("=" * 30 )
 print("SELESAI")
