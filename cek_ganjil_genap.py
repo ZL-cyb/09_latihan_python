06e958e72206c1919ba34b3a00104ea59ae71ae5
ZL-cyb <fahim.tkjsmeksa@gmail.com>
2026-08-29 10:15:05 +0700

    Menambahkan fitur ganjil genap dan bilangan prima

--- cek_ganjil_genap.py
+++ cek_ganjil_genap.py
@@ -1,24 +1,75 @@
-# OPERATOR GANJIL GENAP
-print("=" * 30)
-print("PROGRAM CEK GANJIL/GENAP")
-print("=" * 30)
+#Program Ganjil/Genap
+def cek_ganjil_genap():
+    print("=" * 30)
+    print("PROGRAM CEK GANJIL/GENAP")
+    print("=" * 30)
 
-while True:
-    #meminta input angka dari user
-    angka= int (input("\nMasukan angka :"))
+    while True:
+     #meminta input angka dari user
+        angka= int (input("\nMasukan angka :"))
     
-    #mengecek apakah genap atau ganjil
-    if angka % 2 == 0:
-        print(f"{angka} adalah bilangan GENAP")
-    else:
-        print(f"{angka} adalah bilangan GANJIL")
+     #mengecek apakah genap atau ganjil
+        if angka % 2 == 0:
+            print(f"{angka} adalah bilangan GENAP")
+        else:
+            print(f"{angka} adalah bilangan GANJIL")
         
-    #tanya user ingin mengulang?
-    ulang= input("Apakah mau cek angka lagi? (y/n): ")
+        #tanya user ingin mengulang?
+        ulang= input("Apakah mau cek angka lagi? (y/n): ")
     
-    #jika user mengetik selain 'y' loop akan berhenti
-    if ulang.lower() != 'y':
+        #jika user mengetik selain 'y' loop akan berhenti
+        if ulang.lower() != 'y':
+            break
+    
+# OPERATOR UTAMA
+def cek_bilangan_prima():
+    print("\n" + "=" * 30)
+    print("PROGRAM CEK BILANGAN PRIMA")
+    print("=" * 30)
+    
+    while True:
+        angka = int(input("\nMasukan angka : "))
+        
+        if angka > 1:
+            is_prima = True
+            for i in range(2, angka):
+                if angka % i == 0:
+                    is_prima = False
+                    break
+                
+            if is_prima:
+                print(f"{angka} adalah bilangan PRIMA")
+            else:
+                print(f"{angka} BUKAN bilangan PRIMA")
+        else:
+            print(f"{angka} BUKAN bilangan PRIMA")
+        ulang = input("Apakah mau cek angka lagi? (y/n): ")
+        if ulang.lower() != 'y':
+            break
+        
+# MENU UTAMA
+while True:
+    print("\n" + "=" * 30)
+    print("PILIH PROGRAM")
+    print("=" * 30)
+    print("1. Cek Ganjil / Genap")
+    print("2. Cek Bilangan Prima")
+    print("3. Keluar")
+    print("=" * 30)
+    
+    pilihan = input("Pilih menu (1/2/3) : ")
+   
+    if pilihan == '1':
+       cek_ganjil_genap()
+    elif pilihan == '2':
+        cek_bilangan_prima()
+    elif pilihan =='3':
         break
-    
-print("=" * 30 )
+    else:
+        print("Pilihan tidak valid, silakan coba lagi.")
+        
+print("\n" + "=" * 30)
 print("SELESAI")
+                    
+                    
+    
